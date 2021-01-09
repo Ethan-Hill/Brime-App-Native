e6b38e6a1eeb75792697525f6b743e4d65ddb306
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-09 11:25:38 +0000

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -20,7 +20,7 @@ export default function HomeScreen(props) {
   const navigation = useNavigation();
 
   const gradient = isEnabled
-    ? `linear-gradient(135deg, rgba(69,60,239,1) 0%, rgba(159,56,147,1) 50%, rgba(254,53,54,1) 100%)`
+    ? `linear-gradient(15deg, rgba(69,60,239,1) 0%, rgba(159,56,147,1) 50%, rgba(254,53,54,1) 100%)`
     : `linear-gradient(180deg, rgba(20,21,34,1) 0%, rgba(35,42,78,1) 50%, rgba(20,21,34,1) 100%)`;
 
   return (
